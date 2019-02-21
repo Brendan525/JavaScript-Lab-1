e74a1e489d0f19c14f407e9665aaec5db7a1c4ef
Brendan525 <broe5252@hotmail.com>
2019-02-20 20:52:59 -0500

    Part 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# JavaScript-Lab-1
---
+++ game 2.js
@@ -0,0 +1,51 @@
+let userHealth = 40;
+let grantHealth = 10;
+let wins = 0;
+
+function startGame() {
+
+    const question = prompt("Would you like to play a game?"); 
+        
+    if (question === "yes") {
+                startCombat();
+            }
+}
+
+function startCombat() {
+    const username = prompt(`What is your name?`);
+        console.log(`Grant vs ${username}`);
+
+while (userHealth > 0 && wins < 3) {
+
+ const attack = prompt("Would you like to attack or quit?"); {
+    function getDamage() {
+    return Math.floor(Math.random() * 2) +5;
+    }
+}   
+    
+if (attack === "attack") {
+    userHealth -= getDamage();
+    grantHealth -= getDamage();
+    console.log(`${username} has ${userHealth} health left `)
+    console.log(`Grant has ${grantHealth} health left`)
+ 
+
+ } if (attack === 'quit') {
+     break;
+ }
+
+if (grantHealth <= 0) {
+wins++;
+grantHealth = 10;
+}
+
+if (userHealth <= 0 && win) {
+console.log(`Grant has won.`);
+}
+if (wins === 3){
+    console.log(`${username} has won.`);
+}
+
+}
+
+}
